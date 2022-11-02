2cf0672658c05ca555f78509bb03e26c6ecde0c0
Tattari Meghanath <111589928+TattariMeghanath@users.noreply.github.com>
2022-11-02 17:15:52 -0500

    added selector

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var IntrestedRouter = require('./routes/Intrested');
 var gridbuildRouter = require('./routes/gridbuild');
+var selectordRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Intrested', IntrestedRouter);
 app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectordRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
